f5573a6354b0f96298e21440a2995b1890b44ba4
amritxyz <amrit44404@proton.me>
2025-02-15 22:02:31 +0545

    new scripts #boring life

--- .local/bin/2grim.sh
+++ .local/bin/2grim.sh
@@ -4,22 +4,22 @@
 SAVE_DIR=~/.local/ss
 
 if [ ! -d "$SAVE_DIR" ]; then
-    mkdir -p "$SAVE_DIR"
+	mkdir -p "$SAVE_DIR"
 fi
 
 # Find the highest number used in existing filenames
 MAX_NUMBER=$(ls -1 $SAVE_DIR/*_screenshot.png 2>/dev/null | \
-              awk -F'[_]' '{print $1}' | \
-              awk -F/ '{print $NF}' | \
-              sort -n | \
-              tail -1)
+	awk -F'[_]' '{print $1}' | \
+	awk -F/ '{print $NF}' | \
+	sort -n | \
+	tail -1)
 
 # ls -1 ~/Pictures/*_screenshot.png | awk -F'[_]' '{print $1}' | awk -F/ '{print $NF}' | sort -n | tail -1
 
 # Check if ls found any files
 if [ -z "$MAX_NUMBER" ]; then
-  # If no files were found, set MAX_NUMBER to 0
-  MAX_NUMBER=0
+	# If no files were found, set MAX_NUMBER to 0
+	MAX_NUMBER=0
 fi
 
 # Print debug output
@@ -45,8 +45,8 @@ echo "REGION: $REGION"
 
 # Check if slurp produced any output
 if [ -n "$REGION" ]; then
-  grim -g "$REGION" "$FILENAME"
-  echo "Screenshot saved as $FILENAME"
+	grim -g "$REGION" "$FILENAME"
+	echo "Screenshot saved as $FILENAME"
 else
-  echo "Error: unable to capture region"
+	echo "Error: unable to capture region"
 fi

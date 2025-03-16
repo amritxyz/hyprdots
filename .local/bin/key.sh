29cbd7778507c4b3c0b851565f59a45b34fff447
amritxyz <amrit44404@proton.me>
2025-03-16 12:59:59 +0545

    removed cache section

--- .local/bin/key.sh
+++ .local/bin/key.sh
@@ -1,1 +1,1 @@
-wtype $(grep -v '^#' ~/.local/share/key | wofi --dmenu --lines 4 | cut -d' ' -f1)
+wtype $(grep -v '^#' ~/.local/share/key | wofi --dmenu --lines 4 --width 500 --cache=/dev/null | cut -d' ' -f1)
--- .local/bin/snippets.sh
+++ .local/bin/snippets.sh
@@ -1,1 +1,1 @@
-wtype $(grep -v '^#' ~/.local/share/snippets | wofi --dmenu --lines 8 --width 800 | cut -d' ' -f1)
+wtype $(grep -v '^#' ~/.local/share/snippets | wofi --dmenu --lines 8 --width 800 --cache=/dev/null | cut -d' ' -f1)
